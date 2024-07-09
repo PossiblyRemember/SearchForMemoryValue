c6240446d9646e852f950c4d87acb21f6b97c9ae
Taylan Smith <possiblyremember@gmail.com>
2024-07-09 08:46:08 -0400

    Removed testing parameters.

--- include/memoryMan.h
+++ include/memoryMan.h
@@ -5,102 +5,107 @@
 #include <windows.h>
 #include <psapi.h>
 using namespace std;
-
-char binaryToChar(const char* binary_c_str) {
-	char character = 0;
-	for (unsigned int i = 0; i < 8; i++) {
-		if (binary_c_str[i] == '1') {
-			character |= 1 << (7 - i);
+namespace PRUtils {
+	namespace memory {
+		char binaryToChar(const char* binary_c_str) {
+			char character = 0;
+			for (unsigned int i = 0; i < 8; i++) {
+				if (binary_c_str[i] == '1') {
+					character |= 1 << (7 - i);
+				}
+			}
+			return character;
 		}
-	}
-	return character;
-}
 
-DWORD_PTR GetProcessBaseAddress(HANDLE hProcess) {
-	DWORD_PTR baseAddress = 0;
-	if (hProcess) {
-		HMODULE hMod;
-		DWORD cbNeeded;
-		if (EnumProcessModulesEx(hProcess, &hMod, sizeof(hMod), &cbNeeded,NULL)) {
-			baseAddress = (DWORD_PTR)hMod;
+		DWORD_PTR GetProcessBaseAddress(HANDLE hProcess) {
+			DWORD_PTR baseAddress = 0;
+			if (hProcess) {
+				HMODULE hMod;
+				DWORD cbNeeded;
+				if (EnumProcessModulesEx(hProcess, &hMod, sizeof(hMod), &cbNeeded, NULL)) {
+					baseAddress = (DWORD_PTR)hMod;
+				}
+				else {
+					std::cerr << "PROCESS MODULE FAILED" << std::endl;
+				}
+			}
+			else {
+				std::cerr << "OPEN PROCESS FAILED" << std::endl;
+			}
+			return baseAddress;
 		}
-		else {
-			std::cerr << "PROCESS MODULE FAILED" << std::endl;
-		}
-	}
-	else {
-		std::cerr << "OPEN PROCESS FAILED" << std::endl;
-	}
-	return baseAddress;
-}
 
-template <typename T>
-T searchMemory(size_t size, size_t alignment, T target, int range, int loops) {
-	void* memoryBlock = malloc(size + alignment - 1);
-	size_t runs;
-	bool caught_target = false;
-	uintptr_t alignedAddress = ((uintptr_t)&memoryBlock + alignment - 1) & ~(alignment - 1);
-	for (int i = 0; i < loops; i++) {
+		template <typename T>
+		T *searchMemory(T target, int range, int loops) {
+			size_t size = sizeof(target);
+			size_t alignment = alignment_of_v<T>;
+			void* memoryBlock = malloc(size + alignment - 1);
+			size_t runs;
+			bool caught_target = false;
+			uintptr_t alignedAddress = ((uintptr_t)&memoryBlock + alignment - 1) & ~(alignment - 1);
+			for (int i = 0; i < loops; i++) {
 #ifdef DEBUG
-		cout << "Looping\n" << i % 2 << endl;
+				cout << "Looping\n" << i % 2 << endl;
 #endif
-		if (i % 2 == 0) {
-			runs = 0;
-			for (size_t offset = 0; !caught_target && runs < range; offset += alignment, runs++) {
-				uintptr_t offsetAddress = alignedAddress + offset;
-				T* dataAddress = reinterpret_cast<T*>(offsetAddress);
-				//__try {
-					if (*dataAddress == target) {
-						caught_target = true;
+				if (i % 2 == 0) {
+					runs = 0;
+					for (size_t offset = 0; !caught_target && runs < range; offset += alignment, runs++) {
+						uintptr_t offsetAddress = alignedAddress + offset;
+						T* dataAddress = reinterpret_cast<T*>(offsetAddress);
+						//__try {
+						if (*dataAddress == target) {
+							caught_target = true;
 #ifdef DEBUG
-						cout << "CAUGHT: " << *dataAddress << endl;
+							cout << "CAUGHT: " << *dataAddress << endl;
 #endif
-						return *dataAddress;
+							return dataAddress;
+						}
+#ifdef DEBUG
+						else {
+							cout << "Rerun a " << endl << (void*)offsetAddress << endl << dataAddress << endl;
+						}
+#endif
+						//}
+						//__except (EXCEPTION_ACCESS_VIOLATION or EXCEPTION_EXECUTE_HANDLER) {
+						//	cerr << "RUNTIME ERROR";
+						//}
 					}
+				}
+				else {
+					runs = 0;
+					for (size_t offset = 0; !caught_target && runs < range; offset -= alignment, runs++) {
+						uintptr_t offsetAddress = alignedAddress + offset;
+						T* dataAddress = reinterpret_cast<T*>(offsetAddress);
+						//__try {
+						if (*dataAddress == target) {
+							caught_target = true;
 #ifdef DEBUG
-					else {
-						cout << "Rerun a " << endl << (void*)offsetAddress << endl << dataAddress << endl;
-					}
+							cout << "CAUGHT: " << *dataAddress << endl;
 #endif
-				//}
-				//__except (EXCEPTION_ACCESS_VIOLATION or EXCEPTION_EXECUTE_HANDLER) {
-				//	cerr << "RUNTIME ERROR";
-				//}
+							return dataAddress;
+						}
+#ifdef DEBUG
+						else {
+							cout << "Rerun b " << endl << (void*)offsetAddress << endl << dataAddress << endl;
+						}
+#endif
+						//}
+						//__except (EXCEPTION_ACCESS_VIOLATION or EXCEPTION_EXECUTE_HANDLER) {
+						//	cerr << "RUNTIME ERROR";
+						//}
+					}
+				}
 			}
 		}
-		else {
-			runs = 0;
-			for (size_t offset = 0; !caught_target && runs < range; offset -= alignment, runs++) {
-				uintptr_t offsetAddress = alignedAddress + offset;
-				T* dataAddress = reinterpret_cast<T*>(offsetAddress);
-				//__try {
-					if (*dataAddress == target) {
-						caught_target = true;
-#ifdef DEBUG
-						cout << "CAUGHT: " << *dataAddress << endl;
-#endif
-						return *dataAddress;
-					}
-#ifdef DEBUG
-					else {
-						cout << "Rerun b " << endl << (void*)offsetAddress << endl << dataAddress << endl;
-					}
-#endif
-				//}
-				//__except (EXCEPTION_ACCESS_VIOLATION or EXCEPTION_EXECUTE_HANDLER) {
-				//	cerr << "RUNTIME ERROR";
-				//}
-			}
+
+
+		template <typename T>
+		T searchApplicationMemory(size_t alignment, T target, int range, int loops, DWORD PID) {
+			HANDLE hProcess = OpenProcess(PROCESS_VM_READ, FALSE, PID);
+			DWORD_PTR baseAddress = GetProcessBaseAddress(hProcess);
+			T buffer;
+			ReadProcessMemory(hProcess, (LPCVOID)(baseAddress), &buffer, sizeof(target), NULL);
+			return 0;
 		}
 	}
-}
-
-
-template <typename T>
-T searchApplicationMemory(size_t alignment, T target, int range, int loops, DWORD PID) {
-	HANDLE hProcess = OpenProcess(PROCESS_VM_READ,FALSE,PID);
-	DWORD_PTR baseAddress = GetProcessBaseAddress(hProcess);
-	T buffer;
-	ReadProcessMemory(hProcess, (LPCVOID)(baseAddress), &buffer, sizeof(target), NULL);
-	return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,48 +1,12 @@
 #include <cstdlib>
-#include <cstdio>
-#include <iostream>
 #include <string>
-#include <memory>
-#include <memory.h>
-#include <windows.h>
-#include <thread>
-#include <fstream>
-#include <vector>
-//#include <GLFW/glfw3.h>
-#include <bitset>
-//#include <GL/freeglut.h>
-#pragma comment(lib, "")
-
-//#include "image_utils.h"
-#include "./include/memoryMan.h"
-//#define DEBUG
-#define endl "\n"
-
+#include <iostream>
+#include "include/memoryMan.h"
 using namespace std;
+using namespace PRUtils::memory;
 
-int test = 2;
-
-#pragma pack(push, 1)
-struct BytedStuff {
-	string str = "very specific string";
-};
-#pragma pack(pop)
-
-int working() {
-	BytedStuff newStructInit;
-	int* intPtr = &test;
+int main(int argc, char** argv) {
+	const char* target = argv[1];
 	
-	//DWORD PID = 30884;
-	//HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, PID);
-	//MEMORY_BASIC_INFORMATION memInfo;
-	//cout << (void*)VirtualQueryEx(hProcess, NULL, &memInfo, sizeof(memInfo)) << endl;
-	//cout << memInfo.BaseAddress << endl;
-	//cout << (void*)hProcess << endl;
-	//system("pause");
-	return -1;
-}
-
-int main() {
-	int n = 2;
-	asm(""); 
+	cout << searchMemory<const char*>(target, 5000, 2) << std::endl;
 }
