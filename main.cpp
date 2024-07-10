e285f975f8a209cfe8e776be4650244f50f1b94f
Taylan Smith <possiblyremember@gmail.com>
2024-07-10 02:46:49 -0400

    Successfully either rewrote a pointer value or redid the pointer. Either way, it works.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,30 @@ using namespace std;
 using namespace PRUtils::memory;
 
 int main(int argc, char** argv) {
-	const char* target = argv[1];
-	
-	cout << searchMemory<const char*>(target, 5000, 2) << std::endl;
+	const char* target;
+	int range, loops;
+	if (argc == 2) {
+		target = argv[1];
+	}
+	else {
+		target = "this is a test message";
+	}
+	if (argc == 3) {
+		range = atoi(argv[2]);
+	}
+	else {
+		range = 500;
+	}
+	if (argc == 4) {
+		loops = atoi(argv[3]);
+	}
+	else {
+		loops = 2;
+	}
+
+	cout << searchMemory<const char*>(target, range, loops) << endl;
+	cout << *searchMemory<const char*>(target, range, loops) << endl;
+	*searchMemory<const char*>(target, range, loops) = "test messagehrthjrjtyjfgthj";
+	cout << searchMemory<const char*>("test messagehrthjrjtyjfgthj", range, loops) << endl;
+	cout << *searchMemory<const char*>("test messagehrthjrjtyjfgthj", range, loops) << endl;
 }
